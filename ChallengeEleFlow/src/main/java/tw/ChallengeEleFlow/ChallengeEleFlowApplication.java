9b26fe94fd277e9d830647944cf70951565f8316
Thiago Anjos <thiagowaltrick@gmail.com>
2021-02-26 22:53:10 -0300

    projeto iniciou Java Spring com Swagger

---
+++ ChallengeEleFlow/src/main/java/tw/ChallengeEleFlow/ChallengeEleFlowApplication.java
@@ -0,0 +1,13 @@
+package tw.ChallengeEleFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeEleFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeEleFlowApplication.class, args);
+	}
+
+}
---
+++ ChallengeEleFlow/src/test/java/tw/ChallengeEleFlow/ChallengeEleFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.ChallengeEleFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeEleFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
